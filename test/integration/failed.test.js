1fb9dde2d6a1a9258d51a6e239907873f64dc10a
Jesper Borgstrup <jbo@coinify.com>
2020-01-13 13:10:56 +0100

    TECH-445 Increase test timeout for a test that regularly timed out

--- test/integration/failed.test.js
+++ test/integration/failed.test.js
@@ -58,7 +58,7 @@ describe('Integration tests', () => {
         await rabbit.enqueueTask(fullTaskName, taskContext, enqueueOptions);
         await rabbit.emitEvent(eventName, eventContext, enqueueOptions);
       });
-    });
+    }).timeout(3000);
 
     it('should be able to reenqueue a failed message', async () => {
       return new Promise(async (resolve) => {
